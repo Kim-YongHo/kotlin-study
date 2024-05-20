9696f55fc1b3f3705c5d0a11d98c5843576a14e2
YongHo_Kim <ashore92@naver.com>
2024-05-20 19:22:00 +0900

    240520 - 학습

--- BasicKotlin/Kotlin/05.메소드/실습.kts
+++ BasicKotlin/Kotlin/05.메소드/실습.kts
@@ -1,52 +1,61 @@
-// 산술연산자
-val number1: Int = 1 + 10
-val number2: Int = 2 - 10
-val number3: Int = 2 * 40
-val number4: Int = 40 / 2
-val number5: Int = 45 % 7
-println(number5)
-val number6 = 10 * 427 % 73 + 108 // 할당하려는 값의 타입을 알기 어려운 경우에는 코틀린에게 맡겨도된다
+var number100: Int = 100
 
-// 대입연산자
-var number7: Int = 22
-println(number7)
-number7 = 100
-println(number7)
 
-// 복합대입연산자
-// A+=B -> A = A + B
-var number8: Int = 12
-number8 += 8
-number8 -= 10
-number8 *= 2
-number8 /= 5
-println(number8)
+// 함수를 선언하는 방법
+fun plusNumbers(first_num: Int, second_num: Int): Int {
+    val result: Int = first_num + second_num
+    return result
+}
 
-// 증감연산자 (++, --)
-// ++,-- -> 1씩 더하가나 1씩 뺀다
-var number9: Int = 10
-println(number9++) // 10
-println(number9) // 11
 
-println(++number9) // 11
+// 함수를 사용(호출)하는 방법
+plusNumbers(first_num = 10, second_num = 20) // -> 30
+plusNumbers(10, 20) // ->30
 
-// 비교연산자
-val number10: Int = 2
-val number11: Int = 2
-val result: Boolean = number10 > number11
+val result: Int = plusNumbers(first_num = 5, second_num = 7)
 println(result)
-val result2: Boolean = number10 == number11
-println(result2)
-val result3: Boolean = number10 != number11
-println(result3)
-// ===, !== -> 잘사용하지 않는다
-//  - 주소값이 같은지 물어보는 연산자
-// ==, !=
-//  - 값이 같은지 물어보는 연산
 
-// 논리연사자
-val boolean1: Boolean = false
-val boolean2: Boolean = false
-val result4: Boolean = boolean1 && boolean2
-val result5: Boolean = boolean1 || boolean2
-println(result5)
+// 기본값이 있는 함수를 선언하는 방법
+fun plusNumbersWithDefault(firstNum: Int, secondNum: Int = 10): Int {
+    return firstNum + secondNum
+}
+
+val result2: Int = plusNumbersWithDefault(firstNum = 10)
+println(result2)
+val result3: Int = plusNumbersWithDefault(firstNum = 10, secondNum = 20)
+println(result3)
+
+// 반환값이 없는 함수를 선언하는 방법
+fun plustNumberWithNoReturn(first_num: Int, second_num: Int): Unit {
+    val result: Int = first_num + second_num
+    println(result)
+}
+
+fun plustNumberWithNoReturn2(first_num: Int, second_num: Int) {
+    val result: Int = first_num + second_num
+    println(result)
+}
+
+fun plustNumberWithNoReturn3(first_num: Int, second_num: Int) {
+    val result: Int = first_num + second_num
+    println(result)
+    return
+}
+plustNumberWithNoReturn3(100, 200)
+
+// 함수선언을 간단하게 하는 방법
+fun shortPlusNumbers(firstNum: Int, secondNum: Int) = firstNum + secondNum
+val result10: Int = shortPlusNumbers(firstNum = 10, secondNum = 100)
+println(result10)
+
+
+// 가변인자를 갖는 함수
+fun plusMultipleNumbers(vararg numbers: Int): Unit {
+    for (number in numbers) {
+        println(number)
+    } // numbers에 들어온 값들을 하나하나 돌면서 println을 진행한다
+}
+plusMultipleNumbers(1, 2, 3, 4, 5)
+
+number100 = 200
+println(number100)
